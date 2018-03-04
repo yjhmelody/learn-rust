1d5312c2aa2ca3baf64c54ea057d92fecbba5702
yjhmelody <465402634@qq.com>
2018-03-04 12:27:52 +0800

    add basic web-server

---
+++ web-server/src/main.rs
@@ -0,0 +1,45 @@
+use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::fs::File;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    
+    let get = b"GET / HTTP/1.1\r\n";
+    if buffer.starts_with(get) {
+        let mut file = File::open("hello.html").unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+
+        let response = format!("HTTP/1.1 200 Ok\r\n\r\n{}", contents);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let header = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        let mut file = File::open("404.html").unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+
+        let response = format!("{}{}", header, contents);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
+
+    
+
+    // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+
+}
